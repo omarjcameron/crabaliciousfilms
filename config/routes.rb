3e91ec64721b390749a4f968e00f94996aa72741
Max Peiros <mpeiros@gmail.com>
2017-05-31 21:38:43 -0700

    Add tests for films routing

--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,3 @@ Rails.application.routes.draw do
     resources :ratings, except: [:index, :show]
   end
 end
-
-#           Prefix Verb   URI Pattern                                Controller#Action
-#     film_ratings POST   /films/:film_id/ratings(.:format)          ratings#create
-#  new_film_rating GET    /films/:film_id/ratings/new(.:format)      ratings#new
-# edit_film_rating GET    /films/:film_id/ratings/:id/edit(.:format) ratings#edit
-#      film_rating PATCH  /films/:film_id/ratings/:id(.:format)      ratings#update
-#                  PUT    /films/:film_id/ratings/:id(.:format)      ratings#update
-#                  DELETE /films/:film_id/ratings/:id(.:format)      ratings#destroy
-#            films POST   /films(.:format)                           films#create
-#         new_film GET    /films/new(.:format)                       films#new
-#             film GET    /films/:id(.:format)                       films#show
-#                  DELETE /films/:id(.:format)                       films#destroy
