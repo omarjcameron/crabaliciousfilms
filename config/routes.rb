feeb07dc33c8e34ffac4018f59b68d30762a533c
Omar Cameron <email": null>
2017-05-31 17:04:56 -0700

    Add film destroy action and user plus session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'categories#index'
 
+  resources :users, only: [:index, :new, :show, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :categories, only: [:index, :show]
 
   resources :films, except: [:index, :edit, :update]
