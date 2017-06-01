49e25d8373df9684590582b82a46b45cf1b6fdc5
Max Peiros <mpeiros@gmail.com>
2017-05-31 20:14:12 -0700

    Add validation tests to Comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:comment) { Comment.new(content: 'I am a comment',
+                              user_id: User.first.id, 
+                              review_id: Review.first.id) }
 
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(comment).to be_valid
+    end
+
+    it 'is not valid without content' do
+      comment.content = ''
+      expect(comment).to_not be_valid
+    end
+
+    it 'is not valid without a user id' do
+      comment.user_id = nil
+      expect(comment).to_not be_valid
+    end
+
+    it 'is not valid with a review id' do
+      comment.review_id = nil
+      expect(comment).to_not be_valid
+    end
+  end
 end
