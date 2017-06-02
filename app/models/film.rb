a8988875ddfbc91ed46a50ae83a6116a33719ae4
Omar Cameron <email": null>
2017-06-01 18:44:44 -0700

    Add scope methods to filter Films by most reviewed, highest rated, and category

--- app/models/film.rb
+++ app/models/film.rb
@@ -4,4 +4,22 @@ class Film < ApplicationRecord
   has_many :ratings, dependent: :destroy
 
   validates_presence_of :title
+
+  scope :most_reviewed, -> { joins(:reviews).group("films.id").order("count(reviews) desc").first }
+
+  scope :most_reviewed_list, -> { joins(:reviews).group("films.id").order("count(reviews) desc") }
+
+  scope :top_five_reviewed, -> { joins(:reviews).group("films.id").order("count(reviews) desc").limit(5) }
+
+  scope :by_category, -> (category) { joins(:category).where("categories.name = ?", category).order("title asc") }
+
+  scope :highest_rated, ->  { all.sort_by(&:average_rating).reverse }
+
+  def average_rating
+    if self.ratings.any?
+      (self.ratings.reduce(0) { |sum, rating| (sum + rating.stars) } / (self.ratings.count * 1.0)).round(2)
+    else
+      0
+    end
+  end
 end
