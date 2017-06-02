d5a5effb64ad353ff8a517b1a6c69bb73c9a857e
Mike & Helen Khumthong <pair+mikeytsou.tamietta@devbootcamp.com>
2017-06-01 17:16:10 -0700

    Authorize trusted user functionality, by creating user index view, with corresponding index, update actions.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,17 @@ class User < ApplicationRecord
   has_many :reviewed_films, through: :reviews, source: :film
   has_many :rated_films, through: :ratings, source: :film
   has_many :comments
-  
+
   has_secure_password
 
   validates_presence_of :username, :email
   validates_uniqueness_of :username, :email
-  validates :password, length: { minimum: 6 }
+  validates :password, length: {minimum: 6}
+  validates :authorize_count, numericality: {equal_to: 0}, unless: :trusted
+  validates :authorize_count, numericality: {greater_than_or_equal_to: 0, less_than: 5}, if: :trusted
+
+  scope :non_trusted, -> { where(trusted: false) }
+  scope :trusted, -> { where(trusted: true) }
 
   def review_for_film(film)
     self.reviews.where(film: film)
@@ -21,5 +26,9 @@ class User < ApplicationRecord
 
   def reviewed_and_rated_films
     (reviewed_films + rated_films).uniq
+  end
+
+  def password
+    @password ||= BCrypt::Password.new(password_digest)
   end
 end
