5ab532585a85998674dd0c17cd205a486d0edd11
Max Peiros <mpeiros@gmail.com>
2017-08-02 12:57:08 -0700

    Refactor films controller specs with before eaches

--- spec/controllers/films_controller_spec.rb
+++ spec/controllers/films_controller_spec.rb
@@ -55,43 +55,45 @@ describe FilmsController do
 
   describe 'POST #create' do
     context 'when valid params are passed' do
+      before(:each) do |test|
+        post :create, params: { film: { title: 'Donnie Darko', category: 'Thriller' } } unless test.metadata[:has_request]        
+      end
+
       it 'responds with status code 302' do
-        post :create, params: { film: { title: 'Donnie Darko', category: 'Thriller' } }
         expect(response).to have_http_status 302
       end
 
-      it 'creates a new film in the database' do
+      it 'creates a new film in the database', :has_request do
         expect { post(:create, params: { film: { title: 'Donnie Darko', category: 'Thriller' } }) }.to change(Film, :count).by(1)
       end
 
       it 'assigns the newly created film as @film' do
-        post :create, params: { film: { title: 'Donnie Darko', category: 'Thriller' } }
         expect(assigns(:film)).to eq Film.last
       end
 
       it 'redirects to the category page of the newly created film' do
-        post :create, params: { film: { title: 'Donnie Darko', category: 'Thriller' } }
         expect(response).to redirect_to category_path(Film.last.category)
       end
     end
 
     context 'when invalid params are passed' do
+      before(:each) do |test|
+        post :create, params: { film: { title: '', category: 'Thriller' } } unless test.metadata[:has_request]                
+      end
+
       it 'sets error message that film was not created' do
-        post :create, params: { film: { title: '', category: 'Thriller' } }
         expect(flash[:errors][0]).to eq "Title can't be blank"
       end
 
-      it 'does not create a new film in the database' do
+      it 'does not create a new film in the database', :has_request do
         expect { post(:create, params: { film: { title: '', category: 'Thriller' } }) }.to change(Film, :count).by(0)
       end
 
       it 'assigns the unsaved film as @film' do
-        post :create, params: { film: { title: '', category: 'Thriller' } }
         expect(assigns(:film)).to be_a_new Film
       end
 
       it 'redirects to the new film path' do
-        post :create, params: { film: { title: '', category: 'Thriller' } }
         expect(response).to redirect_to new_film_path
       end
     end
@@ -100,53 +102,54 @@ describe FilmsController do
   describe 'GET #new' do
     before(:each) do
       session[:id] = '1'
+      get :new
     end
     
     it 'responds with status code 200' do
-      get :new
       expect(response).to have_http_status 200
     end
 
     it 'assigns a new film to @film' do
-      get :new
       expect(assigns(:film)).to be_a_new Film
     end
 
     it 'renders the new template' do
-      get :new
       expect(response).to render_template(:new)
     end
   end
 
   describe 'GET #show' do
+    before(:each) do
+      get :show, params: { id: Film.first.id }      
+    end
+
     it 'responds with status code 200' do
-      get :show, params: { id: Film.first.id }
       expect(response).to have_http_status 200
     end
 
     it 'assigns the correct film to @film' do
-      get :show, params: { id: Film.first.id }
       expect(assigns(:film)).to eq Film.first
     end
 
     it 'renders the :show template' do
-      get :show, params: { id: Film.first.id }
       expect(response).to render_template(:show)
     end  
   end  
 
   describe 'DELETE #destroy' do
+    before(:each) do |test|
+      delete :destroy, params: { id: Film.last.id } unless test.metadata[:has_request]      
+    end
+
     it 'responds with status code 302' do
-      delete :destroy, params: { id: Film.last.id }
       expect(response).to have_http_status 302
     end
 
-    it 'destroys the requested film' do
+    it 'destroys the requested film', :has_request do
       expect { delete(:destroy, params: { id: Film.last.id }) }.to change(Film, :count).by(-1)
     end
 
     it 'redirects to the root path' do
-      delete :destroy, params: { id: Film.last.id }
       expect(response).to redirect_to root_path
     end
   end
