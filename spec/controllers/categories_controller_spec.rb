135ba612ee77d0d477be88cbe58892ab3d3560f5
Max Peiros <mpeiros@gmail.com>
2017-08-02 12:30:11 -0700

    Refactor categories controller specs with before eaches

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,35 +2,37 @@ require 'rails_helper'
 
 describe CategoriesController do
   describe 'GET #index' do
+    before(:each) do
+      get :index      
+    end
+
     it 'responds with status code 200' do
-      get :index
       expect(response).to have_http_status 200
     end
 
     it 'assigns all categories to @categories' do
-      get :index
       expect(assigns(:categories)).to eq Category.all
     end
 
     it 'renders the :index template' do
-      get :index
       expect(response).to render_template(:index)
     end  
   end
 
   describe 'GET #show' do
+    before(:each) do
+      get :show, params: { id: Category.first.id }      
+    end
+
     it 'responds with status code 200' do
-      get :show, params: { id: Category.first.id }
       expect(response).to have_http_status 200
     end
 
     it 'assigns the correct category to @category' do
-      get :show, params: { id: Category.first.id }
       expect(assigns(:category)).to eq Category.first
     end
 
     it 'renders the :show template' do
-      get :show, params: { id: Category.first.id }
       expect(response).to render_template(:show)
     end  
   end  
