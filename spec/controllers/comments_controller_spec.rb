0be40fcc7db97410e84e4872f69d90ac007cf2d3
Max Peiros <mpeiros@gmail.com>
2017-08-02 12:28:45 -0700

    Refactor comments controller spec file

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -11,40 +11,37 @@ describe CommentsController do
   describe 'GET #new' do
     before(:each) do
       session[:id] = '1'
+      get :new, params: { review_id: review.id }
     end
 
     it 'responds with status code 200' do
-      get :new, params: { review_id: review.id }
       expect(response).to have_http_status 200
     end
 
     it 'assigns the correct review to @review' do
-      get :new, params: { review_id: review.id }
       expect(assigns(:review)).to eq review
     end
 
     it 'assigns a new comment to @comment' do
-      get :new, params: { review_id: review.id }
       expect(assigns(:comment)).to be_a_new Comment
     end
 
     it 'renders the new template' do
-      get :new, params: { review_id: review.id }
       expect(response).to render_template(:new)
     end
   end
 
   describe 'POST #create' do
     context 'when valid params are passed' do
-      before(:each) do
-        post :create, params: { review_id: review.id, comment: { content: 'This is a comment' } }
+      before(:each) do |test|
+        post :create, params: { review_id: review.id, comment: { content: 'This is a comment' } } unless test.metadata[:has_request]
       end
 
       it 'responds with status code 302' do
         expect(response).to have_http_status 302
       end
 
-      it 'creates a new comment in the database' do
+      it 'creates a new comment in the database', :has_request do
         expect { post(:create, params: { review_id: review.id, comment: { content: 'This is a comment' } }) }.to change(Comment, :count).by(1)
       end
 
@@ -62,15 +59,15 @@ describe CommentsController do
     end
 
     context 'when invalid params are passed' do
-      before(:each) do
-        post :create, params: { review_id: review.id, comment: { content: '' } }
+      before(:each) do |test|
+        post :create, params: { review_id: review.id, comment: { content: '' } } unless test.metadata[:has_request]
       end
 
       it 'sets error message that comment was not created' do
         expect(flash[:errors][0]).to eq "Content can't be blank"
       end
 
-      it 'does not create a new comment in the database' do
+      it 'does not create a new comment in the database', :has_request do
         expect { post(:create, params: { review_id: review.id, comment: { content: '' } }) }.to change(Comment, :count).by(0)
       end
 
@@ -85,63 +82,66 @@ describe CommentsController do
   end
 
   describe 'GET #edit' do
+    before(:each) do
+      get :edit, params: { review_id: first_comment_review.id, id: comment.id }      
+    end
+
     it 'responds with status code 200' do
-      get :edit, params: { review_id: first_comment_review.id, id: comment.id }
       expect(response).to have_http_status 200
     end
 
     it 'assigns the correct review to @review' do
-      get :edit, params: { review_id: first_comment_review.id, id: comment.id }
       expect(assigns(:review)).to eq first_comment_review
     end
 
     it 'assigns the correct comment to @comment' do
-      get :edit, params: { review_id: first_comment_review.id, id: comment.id }
       expect(assigns(:comment)).to eq comment
     end
 
     it 'renders the edit template' do
-      get :edit, params: { review_id: first_comment_review.id, id: comment.id }
       expect(response).to render_template(:edit)
     end
   end
 
   describe 'PATCH #update' do
     context 'when valid params are passed' do
+      before(:each) do |test|
+        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: 'This is a comment' } } unless test.metadata[:has_request]        
+      end
+
       it 'responds with status code 302' do
-        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: 'This is a comment' } }
         expect(response).to have_http_status 302
       end
 
-      it 'updates the comment in the database' do
+      it 'updates the comment in the database', :has_request do
         comment.update_attributes(content: 'New comment')
         patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: 'Edited comment' } }
         expect(assigns(:comment).content).to eq 'Edited comment'
       end
 
       it 'assigns the correct review to @review' do
-        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: 'Edited comment' } }
         expect(assigns(:review)).to eq first_comment_review
       end
 
       it 'assigns the correct comment to @comment' do
-        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: 'Edited comment' } }
         expect(assigns(:comment)).to eq comment
       end
 
       it 'redirects to the film page of the updated comment' do
-        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: 'Edited comment' } }
         expect(response).to redirect_to film_path(first_comment_review.film)
       end
     end
 
     context 'when invalid params are passed' do
+      before(:each) do |test|
+        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: '' } } unless test.metadata[:has_request]        
+      end
+
       it 'sets error message that comment was not created' do
-        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: '' } }
         expect(flash[:errors][0]).to eq "Content can't be blank"
       end
 
-      it 'does not update the comment in the database' do
+      it 'does not update the comment in the database', :has_request do
         comment.update_attributes(content: 'New content')
         patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: '' } }
         comment.reload
@@ -149,29 +149,29 @@ describe CommentsController do
       end
 
       it 'assigns the unsaved comment as @comment' do
-        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: '' } }
         expect(assigns(:comment)).to eq comment
       end
 
       it 'redirects to the edit review comment path' do
-        patch :update, params: { review_id: first_comment_review.id, id: comment.id, comment: { content: '' } }
         expect(response).to redirect_to edit_review_comment_path(first_comment_review, comment)
       end
     end
   end
 
   describe 'DELETE #destroy' do
+    before(:each) do |test|
+      delete :destroy, params: { review_id: first_comment_review.id, id: comment.id } unless test.metadata[:has_request]      
+    end
+
     it 'responds with status code 302' do
-      delete :destroy, params: { review_id: first_comment_review.id, id: comment.id }
       expect(response).to have_http_status 302
     end
 
-    it 'destroys the requested comment' do
+    it 'destroys the requested comment', :has_request do
       expect { delete(:destroy, params: { review_id: first_comment_review.id, id: comment.id }) }.to change(Comment, :count).by(-1)
     end
 
     it 'redirects to the film path' do
-      delete :destroy, params: { review_id: first_comment_review.id, id: comment.id }
       expect(response).to redirect_to film_path(first_comment_review.film)
     end
   end
